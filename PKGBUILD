Narrat/PKGBuilds

commit b91ca5339fd44d9abefaed8aac313232c0658598

7kaa: change to SPDX indentifier
and remove unnecessary $srcdir

.SRCINFO
@@ -5,7 +5,7 @@ pkgbase = 7kaa
 	url = http://7kfans.com/
 	arch = i686
 	arch = x86_64
-	license = GPL2
+	license = GPL-2.0-only
 	depends = openal
 	depends = desktop-file-utils
 	depends = enet

PKGBUILD
@@ -8,7 +8,7 @@ pkgrel=1
 pkgdesc='Seven Kingdoms: Ancient Adversaries is a real-time strategy (RTS) computer game developed by Trevor Chan'
 url='http://7kfans.com/'
 arch=('i686' 'x86_64')
-license=('GPL2')
+license=('GPL-2.0-only')
 depends=('openal' 'desktop-file-utils' 'enet' 'gcc-libs' 'sdl2')
 optdepends=('7kaa-music: for ingame music')
 conflicts=('7kaa-data')
@@ -24,7 +24,7 @@ b2sums=('d71594ed46086e2c5b2c38da7e5dd2ff9eb7c0b89abcd62c7dee295530ab6609923303d
 
 
 build() {
-  cd "${srcdir}/${pkgname}-${pkgver}"
+  cd "${pkgname}-${pkgver}"
   # Add a flag to avoid a desync between versions compiled by GCC14+ and those before
   CXXFLAGS+=" -fexcess-precision=fast"
   # Add flag to unset -Werror=format-security as it fails in two locations in ONEWSENG.cpp
@@ -36,7 +36,7 @@ build() {
 package() {
   # install data files
   install -dm755 "${pkgdir}/opt/7kaa/"
-  cd "${srcdir}/$pkgname-$pkgver/data/"
+  cd "$pkgname-$pkgver/data/"
   cp -r {ENCYC,ENCYC2,IMAGE,RESOURCE,SCENARI2,SCENARIO,SOUND,SPRITE,TUTORIAL} "${pkgdir}/opt/7kaa/"
 
   # fix permissions

commit f8b3e76e92b62d7ad80f9cd3cd9cff68e9dab50e

7kaa: fix license to GPL-2.0-or-later
The provided license doesn't allow later versions.
Thanks again to Infiltrator for noticing the issue introduced with
b91ca53

.SRCINFO
@@ -1,11 +1,11 @@
 pkgbase = 7kaa
 	pkgdesc = Seven Kingdoms: Ancient Adversaries is a real-time strategy (RTS) computer game developed by Trevor Chan
 	pkgver = 2.15.7
-	pkgrel = 2
+	pkgrel = 3
 	url = http://7kfans.com/
 	arch = i686
 	arch = x86_64
-	license = GPL-2.0-only
+	license = GPL-2.0-or-later
 	depends = openal
 	depends = desktop-file-utils
 	depends = enet

PKGBUILD
@@ -4,11 +4,11 @@
 
 pkgname=7kaa
 pkgver=2.15.7
-pkgrel=2
+pkgrel=3
 pkgdesc='Seven Kingdoms: Ancient Adversaries is a real-time strategy (RTS) computer game developed by Trevor Chan'
 url='http://7kfans.com/'
 arch=('i686' 'x86_64')
-license=('GPL-2.0-only')
+license=('GPL-2.0-or-later')
 depends=('openal' 'desktop-file-utils' 'enet' 'gcc-libs' 'sdl2')
 optdepends=('7kaa-music: for ingame music')
 conflicts=('7kaa-data')